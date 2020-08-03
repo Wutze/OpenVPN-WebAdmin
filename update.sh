29713bf7e286a3d3b882b18e519ab84a6b978af4
micro <micromail.home>
2020-08-03 12:44:16 +0200

    es sollte schon die richtige Version in der neuen config stehen

--- update.sh
+++ update.sh
@@ -575,7 +575,7 @@ write_webconfig(){
  * @copyright 2020 OpenVPN-WebAdmin
  * @link			https://github.com/Wutze/OpenVPN-WebAdmin
  * @see				Internal Documentation ~/doc/
- * @version		1.1.0
+ * @version		1.2.0
  * @todo			new issues report here please https://github.com/Wutze/OpenVPN-WebAdmin/issues
  */
 
