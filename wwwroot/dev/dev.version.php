b63f16aff901da588e8cabae9437c931aaa13cbf
Wutze <6650981+Wutze@users.noreply.github.com>
2020-08-30 10:43:29 +0200

    dev user

--- wwwroot/dev/dev.version.php
+++ wwwroot/dev/dev.version.php
@@ -22,5 +22,5 @@
 (stripos($_SERVER['PHP_SELF'], basename(__FILE__)) === false) or die('access denied?');
 
 define('version',"1.4.0");
-define('devel','dev/client');
+define('devel','dev/user');
 define('cpu','maxiP x64');
