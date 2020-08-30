efb7051a61dcba148b38de4d68891fae0c6d76a2
Wutze <6650981+Wutze@users.noreply.github.com>
2020-08-30 12:10:35 +0200

    überflüssige Dateien gelöscht

--- installation/sql/user-update.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `user` CHANGE `user_id` `user_id` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
-ALTER TABLE `log` CHANGE `user_id` `user_id` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
